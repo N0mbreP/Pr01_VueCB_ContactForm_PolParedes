c22c0b6ea0a1512d6de327b38908f2047b366c46
N0mbreP <109170634+N0mbreP@users.noreply.github.com>
2024-10-22 16:32:02 +0000

    Comit 1, tot fet

---
+++ vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import FillContactes from './components/FillContactes.vue'
+import {ref, reactive} from 'vue'
+const llistaContactes=reactive([])
+const nomContacte= ref('')
+const telefonContacte= ref('')
+const afegirContacte=()=>{
+  llistaContactes.push({
+  nom:String=nomContacte,
+  telefon:String=telefonContacte
+})
+}
+</script>
+
+<template>
+  <main>
+  <h1>Llibreta de Contactes</h1>
+  <input type="text" v-model="nomContacte" placeholder="Nom contacte">
+  <input type="text" v-model="telefonContacte" placeholder="Numero de telefon">
+  <button @click="afegirContacte">Afegir contacte</button>
+  <FillContactes :llistaContactesFill="llistaContactes"/>
+
+
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ vue-project/src/components/FillContactes.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props=defineProps({
+    llistaContactesFill: Array
+})
+</script>
+
+<template>
+    <ul>
+      <li v-for="contacte in llistaContactesFill">{{ contacte.nom }}  -  {{ contacte.telefon }} </li>
+    </ul>
+</template>
